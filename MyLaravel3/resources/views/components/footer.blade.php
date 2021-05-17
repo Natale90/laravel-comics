3ff6d0ee84721d60f544a1ecfaaa6b183b2707b8
Natale Capristo <andreacapristo1@gmail.com>
2021-05-17 21:45:32 +0200

    starting footer

--- MyLaravel3/resources/views/components/footer.blade.php
+++ MyLaravel3/resources/views/components/footer.blade.php
@@ -1,6 +1,11 @@
 <footer>
-  
-  <div class="bottom_bar">
+  <div class="inner_container">
 
+    <img src="{{ asset('/storage/assets/dc-logo-bg.png') }}" alt="">
+  </div>
+  <div class="bottom_bar">
+    <div class="inner_container">
+
+    </div>
   </div>
 </footer>
