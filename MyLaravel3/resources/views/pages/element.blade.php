6b7be673a42ca4d62faaf988c86443b008e542dc
Natale Capristo <andreacapristo1@gmail.com>
2021-05-18 17:57:02 +0200

    table in element page

--- MyLaravel3/resources/views/pages/element.blade.php
+++ MyLaravel3/resources/views/pages/element.blade.php
@@ -66,5 +66,103 @@
 
     </section>
 
+    <section class='moreDetails'>
+      <div class="inner_container_element">
+
+        <div class="talent">
+
+          <div class="">
+            <h4>
+              TALENT
+            </h4>
+          </div>
+
+          <div class="ArtBy">
+            <div class="artBy_left">
+              Art by:
+            </div>
+            <div class="">
+              @foreach ($oneElem['artists'] as $artist)
+                <span>
+
+                  @if ($loop->last)
+
+                    {{ $artist }}
+                  @elseif(!$loop->last)
+                    {{ $artist }},
+
+                  @endif
+                </span>
+              @endforeach
+            </div>
+          </div>
+
+          <div class="writers">
+            <div class="writer_left">
+              Written by:
+            </div>
+
+            <div class="">
+              @foreach ($oneElem['writers'] as $writer)
+                <span>
+
+                  @if ($loop->last)
+
+                    {{ $writer }}
+                  @elseif(!$loop->last)
+                    {{ $writer }},
+
+                  @endif
+                </span>
+              @endforeach
+            </div>
+          </div>
+
+        </div>
+
+        <div class="specs">
+          <div class="">
+
+            <h4>
+              Specs
+            </h4>
+
+          </div>
+
+          <div class="serie">
+            <div class="serie_left">
+              Series:
+            </div>
+
+            <div class="serie_details">
+              {{ $oneElem['series'] }}
+            </div>
+          </div>
+
+          <div class="price_details">
+            <div class="price_left">
+              U.S Price:
+            </div>
+
+            <div class="">
+              {{ $oneElem['price'] }}
+            </div>
+          </div>
+
+          <div class="onSale">
+            <div class="sale_left">
+              On Sale Date:
+            </div>
+
+            <div class="">
+              {{ $oneElem['sale_date'] }}
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+
 
   @endsection
